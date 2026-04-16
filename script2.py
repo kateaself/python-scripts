efa8654b692ff2469111f1e550ceaffd662bbcce
kateaself <kateaself@gmail.com>
2026-04-16 18:08:39 -0400

    Create script2.py

---
+++ script2.py
@@ -0,0 +1,9 @@
+# Pseudocode:
+# 1. Ask the user for their name
+# 2. Ask the user for their favorite animal
+# 3. Print a sentence using both answers
+
+name = input("What is your name? ")
+animal = input("What is your favorite animal? ")
+
+print(name + " likes " + animal + "s!")
